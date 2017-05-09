f122642864b05cfbdaf89f12a18dddfecde271f7
Tran Dang <Tran Dang>
2017-05-09 19:54:53 +0700

    09/05/2017

---
+++ app/src/main/java/com/example/admin/androidbasic/SinhVien.java
@@ -0,0 +1,16 @@
+package com.example.admin.androidbasic;
+
+/**
+ * Created by Admin on 09/05/2017.
+ */
+
+public class SinhVien {
+
+    public String HoTen;
+    public  int NamSinh;
+
+    public SinhVien(String hoTen, int namSinh) {
+        HoTen = hoTen;
+        NamSinh = namSinh;
+    }
+}
